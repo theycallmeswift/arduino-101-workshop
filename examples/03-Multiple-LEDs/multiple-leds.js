8e3c9b36e10db7e979d8cc13ef83f6808e61d859
Swift <theycallmeswift@gmail.com>
2013-02-28 09:48:04 -0800

    Refactored layout

--- examples/03-Multiple-LEDs/multiple-leds.js
+++ examples/03-Multiple-LEDs/multiple-leds.js
@@ -5,7 +5,7 @@ board.on("ready", function() {
   var leds = []
     , led;
 
-  for(var i = 9; i <= 13; i++) {
+  for(var i = 2; i <= 9; i++) {
     led = new five.Led(i);
     led.strobe(500);
     leds.push(led);
